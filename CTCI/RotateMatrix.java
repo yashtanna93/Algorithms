377ab403e90a1783e2bf8f54499929db251f836d
Yash Tanna <yashtann@amazon.com>
2024-03-22 00:09:04 -0700

    clean up

---
+++ CTCI/RotateMatrix.java
@@ -0,0 +1,9 @@
+/**
+1.7 Rotate Matrix: Given an image represented by an NxN matrix, where each pixel in the image is 4 bytes, write a method to rotate the image by 90 degrees. Can you do this in place?
+**/
+
+class RotateMatrix {
+	public static void main(String[] args) {
+		System.out.println("Testing for RotateMatrix...");
+	}
+}
